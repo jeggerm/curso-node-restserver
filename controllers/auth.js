6773ecd30c418358a45a875fb6d36f02841a3473
Jorge Egger Mancilla <jegger.trebolit@bancochile.cl>
2023-08-18 18:38:04 -0400

    Fin seccion 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -46,9 +47,59 @@ const login = async(req, res = response) => {
             msg: 'Contacte al admin'
         })
     }
+}
+
+const googleSignIn = async ( req, res = response ) => {
+
+    const { id_token } = req.body;
+
+    try{
+        const { nombre, img, correo} = await googleVerify( id_token );
+        //console.log(googleUser);
+
+        // Verificar si correo ya existe
+        let usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ) {
+            // Crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img, 
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Validar si el usuario está en estado false
+        if ( !usuario.estado ){
+            return res.status(401).json({
+                msg: 'Usuario bloqueado'
+            })
+        }
+        
+        // Generar Json web Token
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    }catch (error){
+        res.status(400).json({
+            ok: false,
+            msg: 'Token no se pudo verificar'
+        })
+    }
+
     
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
